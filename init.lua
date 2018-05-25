0fe9a963671779fcd2b627809d691d8df954e111
benlanyi@hotmail.com <benlanyi@hotmail.com>
2018-05-25 12:26:14 +0800

    communicating with API server complete

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 print("Mod Loaded")
 
-
 local aPlayer = nil
 local boundaries = {
     x1 = 5,
@@ -8,61 +7,82 @@ local boundaries = {
     z1 = 5,
     z2 = -5
 }
+local http_api = minetest.request_http_api()
+if not http_api then
+    print("ERROR: in minetest.conf, this mod must be in secure.http_mods!")
+end
 
-minetest.register_chatcommand("foo", {
-    privs = {
-        interact = true
-    },
-    func = function(name, param)
-        return true, "You said " .. param .. "!"
-    end
-})
+minetest.register_chatcommand(
+    "foo",
+    {
+        privs = {
+            interact = true
+        },
+        func = function(name, param)
+            return true, "You said " .. param .. "!"
+        end
+    }
+)
 
 minetest.chat_send_all("This is a chat message to all players")
 
-
-minetest.register_on_joinplayer(function(player)
-    minetest.chat_send_all("Give a warm welcome to "..player:get_player_name().."!")
-    aPlayer = player
-    aPlayer:setpos({x = 0, y = 10, z = 0})
-end)
-
-minetest.register_on_chat_message(function(name, message)
-	if name == "singleplayer"
-	and message == "home" then
-		minetest.chat_send_all("setting position")
+minetest.register_on_joinplayer(
+    function(player)
+        minetest.chat_send_all("Give a warm welcome to " .. player:get_player_name() .. "!")
+        aPlayer = player
         aPlayer:setpos({x = 0, y = 10, z = 0})
-        local currentPosition = aPlayer:getpos()
-        print(currentPosition.x)
-	end
-end)
 
--- Check boundary so that player cant walk outside designated area
-minetest.register_globalstep(function(dtime)
-    for _, p in ipairs(minetest.get_connected_players()) do
-        local currentPosition = p:getpos()
-        if currentPosition.x > boundaries.x1 then
-            p:setpos({x = currentPosition.x - 0.1, y = currentPosition.y, z = currentPosition.z})
-        end
-        if currentPosition.x < boundaries.x2 then
-            p:setpos({x = currentPosition.x + 0.1, y = currentPosition.y, z = currentPosition.z})
-        end
-        if currentPosition.z > boundaries.z1 then
-            p:setpos({x = currentPosition.x, y = currentPosition.y, z = currentPosition.z - 0.1})
-        end
-        if currentPosition.z < boundaries.z2 then
-            p:setpos({x = currentPosition.x, y = currentPosition.y, z = currentPosition.z + 0.1})
+        http_api.fetch(
+            {
+                url = "http://localhost:3001/test",
+                post_data = '{ "Message" : "message from minetest"}'
+            },
+            function(res)
+                print(res.data)
+            end
+        )
+    end
+)
+
+minetest.register_on_chat_message(
+    function(name, message)
+        if name == "singleplayer" and message == "home" then
+            minetest.chat_send_all("setting position")
+            aPlayer:setpos({x = 0, y = 10, z = 0})
+            local currentPosition = aPlayer:getpos()
+            print(currentPosition.x)
         end
     end
-  end)
+)
+
+-- Check boundary so that player cant walk outside designated area
+minetest.register_globalstep(
+    function(dtime)
+        for _, p in ipairs(minetest.get_connected_players()) do
+            local currentPosition = p:getpos()
+            if currentPosition.x > boundaries.x1 then
+                p:setpos({x = currentPosition.x - 0.1, y = currentPosition.y, z = currentPosition.z})
+            end
+            if currentPosition.x < boundaries.x2 then
+                p:setpos({x = currentPosition.x + 0.1, y = currentPosition.y, z = currentPosition.z})
+            end
+            if currentPosition.z > boundaries.z1 then
+                p:setpos({x = currentPosition.x, y = currentPosition.y, z = currentPosition.z - 0.1})
+            end
+            if currentPosition.z < boundaries.z2 then
+                p:setpos({x = currentPosition.x, y = currentPosition.y, z = currentPosition.z + 0.1})
+            end
+        end
+    end
+)
 
 -- Prevent player from destroying nodes outside of their boundary
 local old_node_dig = minetest.node_dig
 function minetest.node_dig(pos, node, digger)
-	-- if pos. then
-	-- 	return	
+    -- if pos. then
+    -- 	return
     -- else
-        print(pos.x)
-		return old_node_dig(pos, node, digger)
-	-- end
+    print(pos.x)
+    return old_node_dig(pos, node, digger)
+    -- end
 end

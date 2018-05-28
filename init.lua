37fd39617a6334431f902851d722f7cfee5b6d17
benlanyi@hotmail.com <benlanyi@hotmail.com>
2018-05-28 10:43:59 +0800

    fixed up area generation and added trees

--- init.lua
+++ init.lua
@@ -13,12 +13,14 @@ local defaultSpawn = {
     z = 500
 }
 
+-- Player object
 local aPlayer = {
     reference = nil,
     configuration = nil,
     initialised = false
 }
 
+-- Get http api to be able to fetch etc.
 local http_api = minetest.request_http_api()
 if not http_api then
     print("ERROR: in minetest.conf, this mod must be in secure.http_mods!")
@@ -38,6 +40,7 @@ minetest.register_chatcommand(
 
 minetest.chat_send_all("This is a chat message to all players")
 
+-- Sets up player configuration from server data
 minetest.register_on_joinplayer(
     function(player)
         minetest.chat_send_all("Give a warm welcome to " .. player:get_player_name() .. "!")
@@ -66,12 +69,23 @@ minetest.register_on_joinplayer(
     end
 )
 
+-- Set chat message commands
 minetest.register_on_chat_message(
     function(name, message)
         if message == "generatearea" then
             minetest.chat_send_all("generating area")
             generateArea()
-            aPlayer.reference:setpos({x = 0, y = 10, z = 0})
+        end
+        if message == "cleararea" then
+            minetest.chat_send_all("clearing area")
+            clearAreaSurface()
+        end
+        if message == "home" then
+            aPlayer.reference:setpos({x = 0, y = 5, z = 0})
+        end
+        if message == "empty" then
+            minetest.chat_send_all("emptying world")
+            emptyWorld()
         end
     end
 )
@@ -120,21 +134,62 @@ function setPlayerConfiguration(data)
     print("Group Boundary X2 = " .. aPlayer.configuration["Group"]["GroupBoundary"]["X2"])
 end
 
+-- Generates landmass with trees etc.
 function generateArea()
-    local firstLayer = true
-    local blockToGenerate = "default:dirt_with_grass"
+    generateLand(-10, 10, -10, 10)
+    default.grow_new_pine_tree({x = -9, y = 1, z = -9})
+    default.grow_new_apple_tree({x = -8, y = 1, z = -3})
+    default.grow_new_apple_tree({x = -5, y = 1, z = -3})
+    default.grow_new_apple_tree({x = -6, y = 1, z = -6})
+    default.grow_new_apple_tree({x = -4, y = 1, z = -9})
+
+    generateLand(12, 22, -10, 10)
+end
+
+-- Clears surface of land mass, anything above level 0
+function clearAreaSurface()
+    local blockToGenerate = "air"
     for xval = -10, 10, 1 do
-        for yval = -5, 0, 1 do
+        for yval = 1, 30, 1 do
             for zval = -10, 10, 1 do
                 minetest.set_node({x = xval, y = yval, z = zval}, {name = blockToGenerate})
             end
         end
     end
+end
 
-    local firstLayer = true
+-- Generates land mass
+function generateLand(x1, x2, z1, z2)
+    -- Top layer with grass
     local blockToGenerate = "default:dirt_with_grass"
-    for xval = 12, 22, 1 do
-        for yval = -5, 0, 1 do
+    for xval = x1, x2, 1 do
+        for zval = z1, z2, 1 do
+            minetest.set_node({x = xval, y = 0, z = zval}, {name = blockToGenerate})
+        end
+    end
+    -- Middle layer of dirt
+    local blockToGenerate = "default:dirt"
+    for xval = x1, x2, 1 do
+        for yval = -5, -1, 1 do
+            for zval = z1, z2, 1 do
+                minetest.set_node({x = xval, y = yval, z = zval}, {name = blockToGenerate})
+            end
+        end
+    end
+    -- bedrock of obsidian, may need to make indestructable
+    local blockToGenerate = "default:obsidian"
+    for xval = x1, x2, 1 do
+        for zval = z1, z2, 1 do
+            minetest.set_node({x = xval, y = -6, z = zval}, {name = blockToGenerate})
+        end
+    end
+end
+
+-- Destroys land mass
+function emptyWorld()
+    local blockToGenerate = "air"
+    for xval = -10, 10, 1 do
+        for yval = -10, 20, 1 do
             for zval = -10, 10, 1 do
                 minetest.set_node({x = xval, y = yval, z = zval}, {name = blockToGenerate})
             end
